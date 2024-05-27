18bc487b1bfa401d5b851b6b2e67dc38491d1d44
kalatyn <kalakutok@yahoo.com>
2024-05-27 18:30:08 +0200

    1

--- QUantoloop 2.0.1/src/js/script.js
+++ QUantoloop 2.0.1/src/js/script.js
@@ -71,79 +71,49 @@ window.addEventListener("scroll", function () {
   }
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll(".nav-link");
 
+  for (let i = 1; i <= 5; i++) {
+    const moreAbout = document.querySelectorAll(`.more__about${i}`); 
+    const description = document.querySelectorAll(`#card__disc${i}`);
+    const exit = document.querySelectorAll(`.exit__button${i}`);
 
-const moreAbout = document.querySelectorAll(".more__about");
-const discription = document.querySelectorAll("#card__disc");
-const exit = document.querySelectorAll(".exit__button");
-
-moreAbout.forEach((element) => {
-  element.addEventListener("click", () => {
-    discription.forEach((element) => {
-      element.classList.toggle("show");
+    moreAbout.forEach((element) => {
+      element.addEventListener("click", () => {
+        description.forEach((desc) => {
+          desc.classList.toggle("show");
+        });
+        // Добавляем класс блокировки прокрутки к body
+        document.body.classList.add("scroll-lock");
+      });
     });
-    // Добавляем класс блокировки прокрутки к body
-    document.body.classList.add("scroll-lock");
+
+    exit.forEach((element) => {
+      element.addEventListener("click", () => {
+        description.forEach((desc) => {
+          desc.classList.toggle("show");
+        });
+        // Убираем класс блокировки прокрутки у body
+        document.body.classList.remove("scroll-lock");
+      });
+    });
+  }
+
+  links.forEach(link => {
+    link.addEventListener("click", function () {
+      document.body.classList.remove("scroll-lock");
+      discription.forEach((element) => {
+        if(element.classList.contains("show")) {
+          element.classList.remove("show");
+        }
+        
+      });
+    });
   });
 });
 
-exit.forEach((element) => {
-  element.addEventListener("click", () => {
-    discription.forEach((element) => {
-      element.classList.toggle("show");
-    });
-    // Убираем класс блокировки прокрутки у body
-    document.body.classList.remove("scroll-lock");
-  });
-});
 
-const moreAbout2 = document.querySelectorAll(".more__about2");
-const discription2 = document.querySelectorAll("#card__disc2");
-const exit2 = document.querySelectorAll(".exit__button2");
-
-moreAbout2.forEach((element) => {
-  element.addEventListener("click", () => {
-    discription2.forEach((element) => {
-      element.classList.toggle("show");
-    });
-    // Добавляем класс блокировки прокрутки к body
-    document.body.classList.add("scroll-lock");
-  });
-});
-
-exit2.forEach((element) => {
-  element.addEventListener("click", () => {
-    discription2.forEach((element) => {
-      element.classList.toggle("show");
-    });
-    // Убираем класс блокировки прокрутки у body
-    document.body.classList.remove("scroll-lock");
-  });
-});
-
-const moreAbout3 = document.querySelectorAll(".more__about3");
-const discription3 = document.querySelectorAll("#card__disc3");
-const exit3 = document.querySelectorAll(".exit__button3");
-
-moreAbout3.forEach((element) => {
-  element.addEventListener("click", () => {
-    discription3.forEach((element) => {
-      element.classList.toggle("show");
-    });
-    // Добавляем класс блокировки прокрутки к body
-    document.body.classList.add("scroll-lock");
-  });
-});
-
-exit3.forEach((element) => {
-  element.addEventListener("click", () => {
-    discription3.forEach((element) => {
-      element.classList.toggle("show");
-    });
-    // Убираем класс блокировки прокрутки у body
-    document.body.classList.remove("scroll-lock");
-  });
-});
 
 var element = document.getElementById("myFixedElement");
 var scrollStart = 3500; // Начало диапазона
@@ -174,60 +144,7 @@ setTimeout(function () {
   circle.classList.add("fixed-element-text");
 }, 2000);
 
-// window.addEventListener("scroll", function () {
-//   let element = document.querySelector("#scrolling__info");
-//   let boundingTop = element.getBoundingClientRect().top;
-//   let boundingBottom = element.getBoundingClientRect().bottom;
-//   let viewportHeight =
-//     window.innerHeight || document.documentElement.clientHeight;
 
-//   if (boundingTop >= 0 && boundingBottom <= viewportHeight) {
-//     element.style.borderRadius = "0px";
-//   } else if (boundingBottom < 1000) {
-//     let scrollPosition = Math.abs(bounding.top);
-//     let newSize = 100 - scrollPosition * 0.01;
-//     element.style.width = newSize + "%";
-//     let borderRadius = Math.min(40, scrollPosition * 0.04);
-//     element.style.borderRadius = borderRadius + "px";
-//   }
-// });
-
-// let currentlyOpenContainer = null;
-
-// function toggleContainer(container, button) {
-//   button.addEventListener("click", () => {
-//     if (currentlyOpenContainer && currentlyOpenContainer !== container) {
-//       currentlyOpenContainer.style.width = "40%";
-//       currentlyOpenContainer.previousButton.style.transform = "rotate(0deg)";
-//     }
-    
-//     if (container.style.width === "100%") {
-//       container.style.width = "40%";
-//       button.style.transform = "rotate(0deg)";
-//       currentlyOpenContainer = null;
-      
-//     } else {
-//       container.style.width = "100%";
-//       button.style.transform = "rotate(45deg)";
-//       currentlyOpenContainer = container;
-//       currentlyOpenContainer.previousButton = button;
-//     }
-//   });
-// }
-
-// const pairs = [
-//   { container: "#content_item_wrapper1", button: "#open1" },
-//   { container: "#content_item_wrapper2", button: "#open2" },
-//   { container: "#content_item_wrapper3", button: "#open3" },
-//   { container: "#content_item_wrapper4", button: "#open4" },
-//   { container: "#content_item_wrapper5", button: "#open5" },
-// ];
-
-// pairs.forEach(pair => {
-//   const container = document.querySelector(pair.container);
-//   const button = document.querySelector(pair.button);
-//   toggleContainer(container, button);
-// });
 
 document.addEventListener("DOMContentLoaded", function () {
   for (let i = 1; i <= 5; i++) {
@@ -257,10 +174,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
-  
+
 document.addEventListener("DOMContentLoaded", function () {
   const mainContainer = document.querySelector("#main__scrolling__container");
   const contentContainer = document.querySelector("#content_container");
+
+  const links = document.querySelectorAll(".nav-link");
   
 
   let lastScrollTop = 0; // Для отслеживания направления прокрутки
@@ -276,27 +195,26 @@ document.addEventListener("DOMContentLoaded", function () {
       let currentScrollTop = window.scrollY;
 
       if (mainContainerTop <= 10 && !isContentFullyScrolled) {
-          mainContainer.classList.add('fixed');
-          document.body.classList.add("scroll-lock");
-          contentContainer.style.overflowY = "scroll";
+        mainContainer.classList.add('fixed');
+        document.body.classList.add("scroll-lock");
+        contentContainer.style.overflowY = "scroll";
       } else if (isContentFullyScrolled && mainContainerTop <= 10) {
-          mainContainer.classList.remove('fixed');
-          contentContainer.style.overflowY = "hidden";
-          document.body.classList.remove("scroll-lock");
+        mainContainer.classList.remove('fixed');
+        contentContainer.style.overflowY = "hidden";
+        document.body.classList.remove("scroll-lock");
       } else if (mainContainerTop > 10 && isContentAtTop) {
-          mainContainer.classList.remove('fixed');
-          contentContainer.style.overflowY = "hidden";
-          document.body.classList.remove("scroll-lock");
+        mainContainer.classList.remove('fixed');
+        contentContainer.style.overflowY = "hidden";
+        document.body.classList.remove("scroll-lock");
       } else if (currentScrollTop < lastScrollTop && mainContainerTop <= 10 && !isContentAtTop) {
-          mainContainer.classList.add('fixed');
-          document.body.classList.add("scroll-lock");
-          contentContainer.style.overflowY = "scroll";
+        mainContainer.classList.add('fixed');
+        document.body.classList.add("scroll-lock");
+        contentContainer.style.overflowY = "scroll";
       }
 
-      lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; // Для Firefox
   });
 
-  contentContainer.addEventListener("scroll", function () {
+  contentContainer.addEventListener("scroll", function (event) {
       const contentScrollHeight = contentContainer.scrollHeight;
       const contentScrollTop = contentContainer.scrollTop;
       const mainContainerHeight = contentContainer.clientHeight;
@@ -304,17 +222,22 @@ document.addEventListener("DOMContentLoaded", function () {
       const isContentFullyScrolled = contentScrollTop + mainContainerHeight >= contentScrollHeight;
 
       if (isContentFullyScrolled) {
-          contentContainer.style.overflowY = "hidden";
-          mainContainer.classList.remove('fixed');
-          document.body.classList.remove("scroll-lock");
+        contentContainer.style.overflowY = "hidden";
+        mainContainer.classList.remove('fixed');
+        document.body.classList.remove("scroll-lock");
       } else if (contentScrollTop && window.scrollY <= lastScrollTop) {
-          mainContainer.classList.add('fixed');
-          document.body.classList.add("scroll-lock");
-          contentContainer.style.overflowY = "scroll";
+        mainContainer.classList.add('fixed');
+        document.body.classList.add("scroll-lock");
+        contentContainer.style.overflowY = "scroll";
       }
       
   });
+  links.forEach(link => {
+        link.addEventListener("click", function () {
+          mainContainer.classList.remove("fixed");
+          document.body.classList.remove("scroll-lock");
+          contentContainer.style.overflow = "hidden";
+        });
+      });
 
 });
-
-
