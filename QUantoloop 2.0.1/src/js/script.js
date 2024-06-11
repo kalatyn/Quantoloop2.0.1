ddf324c02d5d4e02f1b16bd7145ee786bafed1e3
kalatyn <kalakutok@yahoo.com>
2024-06-11 21:19:32 +0200

    laptop area

--- QUantoloop 2.0.1/src/js/script.js
+++ QUantoloop 2.0.1/src/js/script.js
@@ -439,3 +439,21 @@ numbers.forEach(function(number) {
     }
   });
 });
+
+
+
+window.addEventListener('scroll', function() {
+  let laptop_area = document.querySelector('.laptop_area');
+  let laptop = document.querySelector('.laptop');
+  let laptop_area_top = laptop_area.getBoundingClientRect().top;
+  let laptop_info = document.querySelector('.laptop_info');
+  if (laptop_area_top < window.innerHeight / 3 ) {
+    let scale = 1 - (window.innerHeight / 2 - laptop_area_top) / (window.innerHeight /0.2);
+    scale = Math.max(scale, 0.5);
+
+    let inverseScale = 1 + (1- scale / 0.9);
+
+    laptop.style.transform = `scale(${scale})`;
+    laptop_info.style.transform = `scale(${inverseScale})`;
+  }
+});
